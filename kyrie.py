82842eeb20f0acdf704106f50e7676bf621c31a1
Billy <77754159+billythegoat356@users.noreply.github.com>
2021-09-24 21:51:29 +0200

    Update kyrie.py

--- kyrie.py
+++ kyrie.py
@@ -4,47 +4,18 @@ strings = "abcdefghijklmnopqrstuvwxyz0123456789"
 
 
 
-class Encrypt:
 
-    
+class Kyrie():
+
     def encrypt(e: str):
-        e1 = Encrypt._kyrie(e)
-        return Encrypt._encrypt(e1)
+        e = Kyrie._ekyrie(e)    
+        return Kyrie._encrypt(e)
 
+    def decrypt(e: str):
+        text = Kyrie._decrypt(e)
+        return Kyrie._dkyrie(text)
 
-    def _kyrie(text: str):
-        
-        r = ""
-        for a in text:
-            if a in strings:
-                a = strings[strings.index(a)-1]
-            r += a
-        return r
-
-
-    def _encrypt(text: str):
-        while True:
-            n = randint(3, 150)
-            if n % 4 == 0:
-                break
-        d = randint(3, 15)
-        t = [chr(ord(t)+d) for t in text]
-        r = "".join(
-            "".join(choice(strings) for _ in range(n)) + i
-            for i, _ in zip(t, range(len(t)))
-        )
-        spcar = choice(("_", "-", "%", "&"))
-        r += "".join(choice(strings) for _ in range(int(n/4))) + spcar + str(n) + spcar + "".join(choice(strings) for _ in range(int(n/4))) + "".join(choice(strings) for _ in range(int(n/4))) + spcar + str(d) + spcar + "".join(choice(strings) for _ in range(int(n/4)))
-        return r
-
-
-class KeyEncrypt:
-
-    def encrypt(e: str, key: str):
-        e1 = KeyEncrypt._kyrie(e)
-        return KeyEncrypt._encrypt(e1, key=key)
-
-    def _kyrie(text: str):
+    def _ekyrie(text: str):
 
         r = ""
         for a in text:
@@ -53,31 +24,7 @@ class KeyEncrypt:
             r += a
         return r
 
-    def _encrypt(text: str, key: str):
-        n = sum(ord(i) for i in key)
-        while True:
-            if n % 4 == 0:
-                break
-            n += 1
-        d = 3
-        t = [chr(ord(t)+d) for t in text]
-        r = "".join(
-            "".join(choice(strings) for _ in range(n)) + i
-            for i, _ in zip(t, range(len(t)))
-        )
-        spcar = choice(("_", "-", "%", "&"))
-        return r
-
-
-
-class KeyDecrypt():
-
-
-    def decrypt(e: str, key: str):
-        e1 = KeyDecrypt._decrypt(e, key)
-        return KeyDecrypt._kyrie(e1)
-
-    def _kyrie(text: str):
+    def _dkyrie(text: str):
         r = ""
         for a in text:
             if a in strings:
@@ -87,6 +34,51 @@ class KeyDecrypt():
                 a = strings[i]
             r += a
         return r
+
+    def _encrypt(text: str):
+        t = [chr(ord(t)+5)if t != "\n" else "\n" for t in text]
+        return "".join(
+            "".join(choice(strings) for _ in range(4)) + i
+            for i in t)
+
+
+
+    def _decrypt(text: str):
+        n = 4
+        t = r = ""
+        while True:
+            if len(text) <= n:
+                break
+            t = text[n]
+            text = text[n+1:]
+            r += chr(ord(t)-5) if t != "\n" else "\n"
+        return r
+
+
+
+class Key:
+
+    def encrypt(e: str, key: str):
+        e1 = Kyrie._ekyrie(e)
+        return Key._encrypt(e1, key=key)
+
+    def decrypt(e: str, key: str):
+        text = Key._decrypt(e, key=key)
+        return Kyrie._dkyrie(text)
+
+
+    def _encrypt(text: str, key: str):
+        n = sum(ord(i) for i in key)
+        while True:
+            if n % 4 == 0:
+                break
+            n += 1
+        d = 3
+        t = [chr(ord(t)+d) if t != "\n" else "\n" for t in text]
+        return "".join(
+            "".join(choice(strings) for _ in range(n)) + i
+            for i, _ in zip(t, range(len(t)))
+        )
 
     def _decrypt(text: str, key: str):
 
@@ -103,43 +95,5 @@ class KeyDecrypt():
                 break
             t = text[n]
             text = text[n+1:]
-            r += chr(ord(t)-d)
-        return r
-
-
-class Decrypt():
-
-    def decrypt(e: str):
-        spcar = ""
-        for a in reversed(e):
-            if a not in strings:
-                spcar = a
-                break
-        e1 = Decrypt._decrypt(e, spcar)
-        return Decrypt._kyrie(e1)
-
-    def _kyrie(text: str):
-        r = ""
-        for a in text:
-            if a in strings:
-                i = strings.index(a)+1
-                if i >= len(strings):
-                    i = 0
-                a = strings[i]
-            r += a
-        return r
-
-    def _decrypt(text: str, spcar: str):
-        n = int(text.split(spcar)[1])
-        d = int(text.split(spcar)[3])
-        text = text.split(spcar)[0] + \
-            text.split(spcar)[2] + text.split(spcar)[4]
-        t = ""
-        r = ""
-        while True:
-            if len(text) == n:
-                break
-            t = text[n]
-            text = text[n+1:]
-            r += chr(ord(t)-d)
+            r += chr(ord(t)-d) if t != "\n" else "\n"
         return r
